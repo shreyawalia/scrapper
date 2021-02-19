4c528b11e26837dd2489c44a0138d086bea81ab1
shreyawalia <37468043+shreyawalia@users.noreply.github.com>
2021-02-19 02:07:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -375,11 +375,3 @@ def transform_view():
     response = make_response(result)
     response.headers["Content-Disposition"] = "attachment; filename=result.csv"
     return (response)
-
-    
-def open_browser():
-      webbrowser.open_new('http://127.0.0.1:5000/')
-
-if __name__ == "__main__":
-      Timer(1, open_browser).start();
-      app.run(port=5000)
